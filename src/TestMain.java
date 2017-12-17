59f516b29e3b30d1d2765d2231b8c914e4d9c963
KrzysztofKita <191874@CARBON.CTI.P.LODZ.PL>
2017-12-17 17:11:31 +0100

    Unit tests for Main

---
+++ src/TestMain.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestMain {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Main main = new Main();
+	}
+
+	@Test
+	void testCreateWindow() {
+	
+		assertNotNull(true);
+	}
+
+}
