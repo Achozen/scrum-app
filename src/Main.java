94c1f87afa35fa17f1b0974e209ac4e90136b1b7
191838 <191838@CARBON.CTI.P.LODZ.PL>
2017-12-17 17:14:24 +0100

    first list created

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,38 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class Main {
+	private static JFrame frame;
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		createWindow();
-
 	}
 
 	private static void createWindow() {
-		JFrame frame = new JFrame("Main Window");
+		frame = new JFrame("Main Window");
+
+		setupWindow(frame);
+		createList(frame);
+		
+		frame.setVisible(true);
+	}
+	
+	private static void setupWindow(JFrame frame) {
 		int width = 500;
 		int height = 500;
 		frame.setSize(width, height);
 		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		createList(frame);
-		
-		
-		frame.setVisible(true);
 	}
 
 	private static void createList(JFrame frame) {
-		int numbers[] = { 1, 2, 3, 4, 5};
 		JComboBox<Integer> myNumbers = new JComboBox<Integer>();
+
 		myNumbers.setBounds(50, 100, 90, 20);
+		
+		for(int i = 1; i<=20; i++) {
+			myNumbers.addItem(i);
+		}
 		frame.add(myNumbers);
 	}
-	
 }
