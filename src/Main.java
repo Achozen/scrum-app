4e085aa468e33ba0898900a641004cdd242ef830
191838 <191838@CARBON.CTI.P.LODZ.PL>
2017-12-17 15:50:46 +0100

    Empty application window

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.awt.Frame;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		createWindow();
+
 	}
 
+	private static void createWindow() {
+		JFrame frame = new JFrame("Main Window");
+		int width = 500;
+		int height = 500;
+		frame.setSize(width, height);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
