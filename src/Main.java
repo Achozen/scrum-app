1685033cd10a17fc6904bdac26f2e0f6e56a60b5
Achozen <achozen93@gmail.com>
2018-01-14 16:16:53 +0100

    First list (#3)
    * emptylist
    
    * first list created
    
    * Merge master to first_list.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,39 @@
-import java.awt.Frame;
-
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class Main {
-	static JFrame frame;
-	
+	private static JFrame frame;
+
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		createWindow();
-
 	}
 
 	private static void createWindow() {
 		frame = new JFrame("Main Window");
+
+		setupWindow(frame);
+		createList(frame);
+
+		frame.setVisible(true);
+	}
+
+	private static void setupWindow(JFrame frame) {
 		int width = 500;
 		int height = 500;
 		frame.setSize(width, height);
-		frame.setVisible(true);
+		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private static void createList(JFrame frame) {
+		JComboBox<Integer> myNumbers = new JComboBox<Integer>();
+
+		myNumbers.setBounds(50, 100, 90, 20);
+
+		for(int i = 1; i<=20; i++) {
+			myNumbers.addItem(i);
+		}
+		frame.add(myNumbers);
 	}
 }
---
+++ src/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Main main = new Main();
+    }
+
+    @Test
+    public void testCreateWindow() {
+
+        assertNotNull(true);
+    }
+}
