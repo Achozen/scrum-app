76aa70eef52e3ebea3e779df86c6e572f5eee457
Aylszen <aylszen@gmail.com>
2018-01-21 15:36:39 +0100

    Travis fix?

---
+++ src/main/java/app/NumberGenerator.java
@@ -0,0 +1,17 @@
+package app;
+import java.util.Random;
+
+public class NumberGenerator {
+
+	public int[] generateNumbers() {
+
+		int[] tab = new int[20];
+		Random generator = new Random();
+
+		for (int i = 0; i < 20; i++) {
+			tab[i] = generator.nextInt(100);
+			System.out.println("Liczba:" + tab[i]);
+		}
+		return tab;
+	}
+}
---
+++ src/test/java/MainnTest.java
@@ -0,0 +1,18 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+import app.*;
+
+public class MainnTest extends TestCase{
+
+    public void setUp() throws Exception {
+        Main main = new Main();
+    }
+
+    public void testCreateWindow() {
+
+        assertNotNull(true);
+    }
+}
